22d3b03afe9160f36a5d58f9e17822abf64cfe38
amitdutta <duttaamit356@gmail.com>
2024-11-12 21:26:09 +0530

    Created login api

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -58,3 +58,78 @@ export async function registerUserController(request,response){
         })
     }
 }
+
+export async function loginController(request,response){
+    try {
+        const { email , password } = request.body
+
+
+        if(!email || !password){
+            return response.status(400).json({
+                message : "provide email, password",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return response.status(400).json({
+                message : "User not register",
+                error : true,
+                success : false
+            })
+        }
+
+        if(user.status !== "Active"){
+            return response.status(400).json({
+                message : "Contact to Admin",
+                error : true,
+                success : false
+            })
+        }
+
+        const checkPassword = await bcryptjs.compare(password,user.password)
+
+        if(!checkPassword){
+            return response.status(400).json({
+                message : "Check your password",
+                error : true,
+                success : false
+            })
+        }
+
+        const accesstoken = await generatedAccessToken(user._id)
+        const refreshToken = await genertedRefreshToken(user._id)
+
+        const updateUser = await UserModel.findByIdAndUpdate(user?._id,{
+            last_login_date : new Date()
+        })
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+        response.cookie('accessToken',accesstoken,cookiesOption)
+        response.cookie('refreshToken',refreshToken,cookiesOption)
+
+        return response.json({
+            message : "Login successfully",
+            error : false,
+            success : true,
+            data : {
+                accesstoken,
+                refreshToken
+            }
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
