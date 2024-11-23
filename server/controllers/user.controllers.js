16995313a642f15cfd6780093a44706d7e0a1008
amitdutta <duttaamit356@gmail.com>
2024-11-23 22:18:57 +0530

    made changes

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,4 +1,6 @@
-export async function registerUserController(request,response){
+import UserModel from '../models/user.model.js'
+
+export async function registerUserController(req,res){
     try {
         const { name, email , password } = request.body
 
@@ -59,109 +61,109 @@ export async function registerUserController(request,response){
     }
 }
 
-export async function loginController(request,response){
-    try {
-        const { email , password } = request.body
+// export async function loginController(request,response){
+//     try {
+//         const { email , password } = request.body
 
 
-        if(!email || !password){
-            return response.status(400).json({
-                message : "provide email, password",
-                error : true,
-                success : false
-            })
-        }
+//         if(!email || !password){
+//             return response.status(400).json({
+//                 message : "provide email, password",
+//                 error : true,
+//                 success : false
+//             })
+//         }
 
-        const user = await UserModel.findOne({ email })
+//         const user = await UserModel.findOne({ email })
 
-        if(!user){
-            return response.status(400).json({
-                message : "User not register",
-                error : true,
-                success : false
-            })
-        }
+//         if(!user){
+//             return response.status(400).json({
+//                 message : "User not register",
+//                 error : true,
+//                 success : false
+//             })
+//         }
 
-        if(user.status !== "Active"){
-            return response.status(400).json({
-                message : "Contact to Admin",
-                error : true,
-                success : false
-            })
-        }
+//         if(user.status !== "Active"){
+//             return response.status(400).json({
+//                 message : "Contact to Admin",
+//                 error : true,
+//                 success : false
+//             })
+//         }
 
-        const checkPassword = await bcryptjs.compare(password,user.password)
+//         const checkPassword = await bcryptjs.compare(password,user.password)
 
-        if(!checkPassword){
-            return response.status(400).json({
-                message : "Check your password",
-                error : true,
-                success : false
-            })
-        }
+//         if(!checkPassword){
+//             return response.status(400).json({
+//                 message : "Check your password",
+//                 error : true,
+//                 success : false
+//             })
+//         }
 
-        const accesstoken = await generatedAccessToken(user._id)
-        const refreshToken = await genertedRefreshToken(user._id)
+//         const accesstoken = await generatedAccessToken(user._id)
+//         const refreshToken = await genertedRefreshToken(user._id)
 
-        const updateUser = await UserModel.findByIdAndUpdate(user?._id,{
-            last_login_date : new Date()
-        })
+//         const updateUser = await UserModel.findByIdAndUpdate(user?._id,{
+//             last_login_date : new Date()
+//         })
 
-        const cookiesOption = {
-            httpOnly : true,
-            secure : true,
-            sameSite : "None"
-        }
-        response.cookie('accessToken',accesstoken,cookiesOption)
-        response.cookie('refreshToken',refreshToken,cookiesOption)
+//         const cookiesOption = {
+//             httpOnly : true,
+//             secure : true,
+//             sameSite : "None"
+//         }
+//         response.cookie('accessToken',accesstoken,cookiesOption)
+//         response.cookie('refreshToken',refreshToken,cookiesOption)
 
-        return response.json({
-            message : "Login successfully",
-            error : false,
-            success : true,
-            data : {
-                accesstoken,
-                refreshToken
-            }
-        })
+//         return response.json({
+//             message : "Login successfully",
+//             error : false,
+//             success : true,
+//             data : {
+//                 accesstoken,
+//                 refreshToken
+//             }
+//         })
 
-    } catch (error) {
-        return response.status(500).json({
-            message : error.message || error,
-            error : true,
-            success : false
-        })
-    }
-}
+//     } catch (error) {
+//         return response.status(500).json({
+//             message : error.message || error,
+//             error : true,
+//             success : false
+//         })
+//     }
+// }
 
-//logout controller
-export async function logoutController(request,response){
-    try {
-        const userid = request.userId //middleware
+// //logout controller
+// export async function logoutController(request,response){
+//     try {
+//         const userid = request.userId //middleware
 
-        const cookiesOption = {
-            httpOnly : true,
-            secure : true,
-            sameSite : "None"
-        }
+//         const cookiesOption = {
+//             httpOnly : true,
+//             secure : true,
+//             sameSite : "None"
+//         }
 
-        response.clearCookie("accessToken",cookiesOption)
-        response.clearCookie("refreshToken",cookiesOption)
+//         response.clearCookie("accessToken",cookiesOption)
+//         response.clearCookie("refreshToken",cookiesOption)
 
-        const removeRefreshToken = await UserModel.findByIdAndUpdate(userid,{
-            refresh_token : ""
-        })
+//         const removeRefreshToken = await UserModel.findByIdAndUpdate(userid,{
+//             refresh_token : ""
+//         })
 
-        return response.json({
-            message : "Logout successfully",
-            error : false,
-            success : true
-        })
-    } catch (error) {
-        return response.status(500).json({
-            message : error.message || error,
-            error : true,
-            success : false
-        })
-    }
-}
+//         return response.json({
+//             message : "Logout successfully",
+//             error : false,
+//             success : true
+//         })
+//     } catch (error) {
+//         return response.status(500).json({
+//             message : error.message || error,
+//             error : true,
+//             success : false
+//         })
+//     }
+// }
