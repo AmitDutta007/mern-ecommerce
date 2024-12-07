0632ecd68ae871f4ff6b636432d3914b75fbfa33
amitdutta <duttaamit356@gmail.com>
2024-12-07 20:58:04 +0530

    added register form functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import toast, { Toaster } from 'react-hot-toast';
+
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
         <Outlet />
       </main>
       <Footer />
+      <Toaster/>
     </>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { FaRegEyeSlash } from "react-icons/fa6";
 import { FaRegEye } from "react-icons/fa6";
-// import toast from 'react-hot-toast';
+import toast from 'react-hot-toast';
 // import Axios from '../utils/Axios';
-// import SummaryApi from '../common/SummaryApi';
+import SummaryApi from '../common/SummaryApi';
 // import AxiosToastError from '../utils/AxiosToastError';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -32,51 +32,51 @@ const Register = () => {
     const valideValue = Object.values(data).every(el => el)
 
 
-    // const handleSubmit = async(e)=>{
-    //     e.preventDefault()
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
 
-    //     if(data.password !== data.confirmPassword){
-    //         toast.error(
-    //             "password and confirm password must be same"
-    //         )
-    //         return
-    //     }
+        if(data.password !== data.confirmPassword){
+            toast.error(
+                "password and confirm password must be same"
+            )
+            return
+        }
 
-    //     try {
-    //         const response = await Axios({
-    //             ...SummaryApi.register,
-    //             data : data
-    //         })
+        // try {
+        //     const response = await Axios({
+        //         ...SummaryApi.register,
+        //         data : data
+        //     })
             
-    //         if(response.data.error){
-    //             toast.error(response.data.message)
-    //         }
+        //     if(response.data.error){
+        //         toast.error(response.data.message)
+        //     }
 
-    //         if(response.data.success){
-    //             toast.success(response.data.message)
-    //             setData({
-    //                 name : "",
-    //                 email : "",
-    //                 password : "",
-    //                 confirmPassword : ""
-    //             })
-    //             navigate("/login")
-    //         }
+        //     if(response.data.success){
+        //         toast.success(response.data.message)
+        //         setData({
+        //             name : "",
+        //             email : "",
+        //             password : "",
+        //             confirmPassword : ""
+        //         })
+        //         navigate("/login")
+        //     }
 
-    //     } catch (error) {
-    //         AxiosToastError(error)
-    //     }
+        // } catch (error) {
+        //     // AxiosToastError(error)
+        // }
 
 
 
-    // }
+    }
     return (
         <section className='w-full container mx-auto px-2'>
             <div className='bg-white my-4 w-full max-w-lg mx-auto rounded p-7'>
                 <p>Welcome to GrabVault</p>
 
                 <form className='grid gap-4 mt-6' 
-                //    onSubmit={handleSubmit}
+                   onSubmit={handleSubmit}
                    >
                     <div className='grid gap-1'>
                         <label htmlFor='name'>Name :</label>
@@ -115,7 +115,7 @@ const Register = () => {
                                 onChange={handleChange}
                                 placeholder='Enter your password'
                             />
-                            {/* <div onClick={() => setShowPassword(preve => !preve)} className='cursor-pointer'>
+                            <div onClick={() => setShowPassword(prev => !prev)} className='cursor-pointer'>
                                 {
                                     showPassword ? (
                                         <FaRegEye />
@@ -123,7 +123,7 @@ const Register = () => {
                                         <FaRegEyeSlash />
                                     )
                                 }
-                            </div> */}
+                            </div>
                         </div>
                     </div>
                     <div className='grid gap-1'>
@@ -150,7 +150,8 @@ const Register = () => {
                         </div>
                     </div>
 
-                    <button disabled={!valideValue} className={` ${valideValue ? "bg-green-800 hover:bg-green-700" : "bg-gray-500" }    text-white py-2 rounded font-semibold my-3 tracking-wide`}>Register</button>
+                    <button 
+                    disabled={!valideValue} className={` ${valideValue ? "bg-green-800 hover:bg-green-700" : "bg-gray-500" }    text-white py-2 rounded font-semibold my-3 tracking-wide`}>Register</button>
 
                 </form>
 
