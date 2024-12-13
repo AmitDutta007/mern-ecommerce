9a42c3e4cc9b29e934c25ae3241e9cc15b3d227a
amitdutta <duttaamit356@gmail.com>
2024-12-13 13:32:06 +0530

    created dashboard page

--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -7,9 +7,9 @@ import Register from "../pages/Register";
 import ForgotPassword from "../pages/ForgotPassword";
 import OtpVerification from "../pages/OtpVerification";
 import ResetPassword from "../pages/ResetPassword";
-// import UserMenuMobile from "../pages/UserMenuMobile";
-// import Dashboard from "../layouts/Dashboard";
-// import Profile from "../pages/Profile";
+import UserMenuMobile from "../pages/UserMenuMobile";
+import Dashboard from "../layouts/Dashboard";
+import Profile from "../pages/Profile";
 // import MyOrders from "../pages/MyOrders";
 // import Address from "../pages/Address";
 // import CategoryPage from "../pages/CategoryPage";
@@ -37,65 +37,65 @@ const router = createBrowserRouter([
                 path: "search",
                 element: <SearchPage />
             },
-            
+
             {
-                path : 'login',
-                element : <Login/>
+                path: 'login',
+                element: <Login />
             },
             {
-                path : "register",
-                element : <Register/>
+                path: "register",
+                element: <Register />
             },
             {
-                path : "forgot-password",
-                element : <ForgotPassword/>
+                path: "forgot-password",
+                element: <ForgotPassword />
             },
             {
-                path : "verification-otp",
-                element : <OtpVerification/>
+                path: "verification-otp",
+                element: <OtpVerification />
             },
             {
-                path : "reset-password",
-                element : <ResetPassword/>
+                path: "reset-password",
+                element: <ResetPassword />
             },
-            // {
-            //     path : "user",
-            //     element : <UserMenuMobile/>
-            // },
-            // {
-            //     path : "dashboard",
-            //     element : <Dashboard/>,
-            //     children : [
-            //         {
-            //             path : "profile",
-            //             element : <Profile/>
-            //         },
-            //         {
-            //             path : "myorders",
-            //             element : <MyOrders/>
-            //         },
-            //         {
-            //             path : "address",
-            //             element : <Address/>
-            //         },
-            //         {
-            //             path : 'category',
-            //             element : <AdminPermision><CategoryPage/></AdminPermision>
-            //         },
-            //         {
-            //             path : "subcategory",
-            //             element : <AdminPermision><SubCategoryPage/></AdminPermision>
-            //         },
-            //         {
-            //             path : 'upload-product',
-            //             element : <AdminPermision><UploadProduct/></AdminPermision>
-            //         },
-            //         {
-            //             path : 'product',
-            //             element : <AdminPermision><ProductAdmin/></AdminPermision>
-            //         }
-            //     ]
-            // },
+            {
+                path: "user",
+                element: <UserMenuMobile />
+            },
+            {
+                path: "dashboard",
+                element: <Dashboard />,
+                children: [
+                    {
+                        path: "profile",
+                        element: <Profile />
+                    },
+                    //         {
+                    //             path : "myorders",
+                    //             element : <MyOrders/>
+                    //         },
+                    //         {
+                    //             path : "address",
+                    //             element : <Address/>
+                    //         },
+                    //         {
+                    //             path : 'category',
+                    //             element : <AdminPermision><CategoryPage/></AdminPermision>
+                    //         },
+                    //         {
+                    //             path : "subcategory",
+                    //             element : <AdminPermision><SubCategoryPage/></AdminPermision>
+                    //         },
+                    //         {
+                    //             path : 'upload-product',
+                    //             element : <AdminPermision><UploadProduct/></AdminPermision>
+                    //         },
+                    //         {
+                    //             path : 'product',
+                    //             element : <AdminPermision><ProductAdmin/></AdminPermision>
+                    //         }
+                ]
+            },
             // {
             //     path : ":category",
             //     children : [
