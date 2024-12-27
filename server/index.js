033fc83210f1e7931a6e6ea0b17ca4d0c48b8cde
amitdutta <duttaamit356@gmail.com>
2024-12-27 10:05:56 +0530

    created subcategory api and get subcategory api

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import connectDB from './config/connectDB.js';
 import userRouter from './route/user.route.js';
 import categoryRouter from './route/category.route.js';
 import uploadRouter from './route/upload.router.js';
+import subCategoryRouter from './route/subCategory.router.js'
 
 dotenv.config();
 const app = express();
@@ -36,6 +37,7 @@ app.get("/", (req, res) => {
 app.use('/api/user', userRouter)
 app.use("/api/category",categoryRouter)
 app.use("/api/file",uploadRouter)
+app.use("/api/subcategory",subCategoryRouter)
 
 
 connectDB().then(() => {
