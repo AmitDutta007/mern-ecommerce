370f409d60997eb8fe1af6400d92b2393e31c89a
amitdutta <duttaamit356@gmail.com>
2025-01-12 20:07:56 +0530

    modified upload product ui

--- client/src/pages/UploadProduct.jsx
+++ client/src/pages/UploadProduct.jsx
@@ -1,6 +1,100 @@
-import ViewImage from "../components/ViewImage"
+import React, { useState } from 'react'
+import { FaCloudUploadAlt } from "react-icons/fa";
+import uploadImage from '../utils/UploadImage';
+import Loading from '../components/Loading';
+import ViewImage from '../components/ViewImage';
+import { MdDelete } from "react-icons/md";
+import { useSelector } from 'react-redux'
+import { IoClose } from "react-icons/io5";
+// import AddFieldComponent from '../components/AddFieldComponent';
+import Axios from '../utils/Axios';
+import SummaryApi from '../common/SummaryApi';
+import AxiosToastError from '../utils/AxiosToastError';
+
+
+
 
 const UploadProduct = () => {
+  const [data, setData] = useState({
+    name: "",
+    image: [],
+    category: [],
+    subCategory: [],
+    unit: "",
+    stock: "",
+    price: "",
+    discount: "",
+    description: "",
+    more_details: {},
+  })
+  const [imageLoading, setImageLoading] = useState(false)
+  const [ViewImageURL, setViewImageURL] = useState("")
+  const allCategory = useSelector(state => state.product.allCategory)
+  const [selectCategory, setSelectCategory] = useState("")
+  const [selectSubCategory, setSelectSubCategory] = useState("")
+  const allSubCategory = useSelector(state => state.product.allSubCategory)
+
+  const [openAddField, setOpenAddField] = useState(false)
+  const [fieldName, setFieldName] = useState("")
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value
+      }
+    })
+  }
+
+  const handleUploadImage = async (e) => {
+    const file = e.target.files[0]
+
+    if (!file) {
+      return
+    }
+    setImageLoading(true)
+    const response = await uploadImage(file)
+    const { data: ImageResponse } = response
+    const imageUrl = ImageResponse.data.url
+
+    setData((preve) => {
+      return {
+        ...preve,
+        image: [...preve.image, imageUrl]
+      }
+    })
+    setImageLoading(false)
+
+  }
+  const handleDeleteImage = async (index) => {
+    data.image.splice(index, 1)
+    setData((preve) => {
+      return {
+        ...preve
+      }
+    })
+  }
+
+  const handleRemoveCategory = async (index) => {
+    data.category.splice(index, 1)
+    setData((preve) => {
+      return {
+        ...preve
+      }
+    })
+  }
+  const handleRemoveSubCategory = async (index) => {
+    data.subCategory.splice(index, 1)
+    setData((preve) => {
+      return {
+        ...preve
+      }
+    })
+  }
+
   return (
     <section className=''>
       <div className='p-2   bg-white shadow-md flex items-center justify-between'>
@@ -8,7 +102,7 @@ const UploadProduct = () => {
       </div>
       <div className='grid p-3'>
         <form className='grid gap-4'
-          // onSubmit={handleSubmit}
+        // onSubmit={handleSubmit}
         >
           <div className='grid gap-1'>
             <label htmlFor='name' className='font-medium'>Name</label>
@@ -17,8 +111,8 @@ const UploadProduct = () => {
               type='text'
               placeholder='Enter product name'
               name='name'
-              // value={data.name}
-              // onChange={handleChange}
+              value={data.name}
+              onChange={handleChange}
               required
               className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
             />
@@ -30,8 +124,8 @@ const UploadProduct = () => {
               type='text'
               placeholder='Enter product description'
               name='description'
-              // value={data.description}
-              // onChange={handleChange}
+              value={data.description}
+              onChange={handleChange}
               required
               multiple
               rows={3}
@@ -43,14 +137,15 @@ const UploadProduct = () => {
             <div>
               <label htmlFor='productImage' className='bg-blue-50 h-24 border rounded flex justify-center items-center cursor-pointer'>
                 <div className='text-center flex justify-center items-center flex-col'>
-                  {/* {
-                              imageLoading ?  <Loading/> : (
-                                <>
-                                   <FaCloudUploadAlt size={35}/>
-                                   <p>Upload Image</p>
-                                </>
-                              )
-                            } */}
+                  {
+                    imageLoading ?
+                      <Loading /> : (
+                        <>
+                          {/* <FaCloudUploadAlt size={35}/> */}
+                          <p>Upload Image</p>
+                        </>
+                      )
+                  }
                 </div>
                 <input
                   type='file'
@@ -62,23 +157,23 @@ const UploadProduct = () => {
               </label>
               {/**display uploded image*/}
               <div className='flex flex-wrap gap-4'>
-                {/* {
-                          data.image.map((img,index) =>{
-                              return(
-                                <div key={img+index} className='h-20 mt-1 w-20 min-w-20 bg-blue-50 border relative group'>
-                                  <img
-                                    src={img}
-                                    alt={img}
-                                    className='w-full h-full object-scale-down cursor-pointer' 
-                                    onClick={()=>setViewImageURL(img)}
-                                  />
-                                  <div onClick={()=>handleDeleteImage(index)} className='absolute bottom-0 right-0 p-1 bg-red-600 hover:bg-red-600 rounded text-white hidden group-hover:block cursor-pointer'>
-                                    <MdDelete/>
-                                  </div>
-                                </div>
-                              )
-                          })
-                        } */}
+                {
+                  data.image.map((img, index) => {
+                    return (
+                      <div key={img + index} className='h-20 mt-1 w-20 min-w-20 bg-blue-50 border relative group'>
+                        <img
+                          src={img}
+                          alt={img}
+                          className='w-full h-full object-scale-down cursor-pointer'
+                          onClick={() => setViewImageURL(img)}
+                        />
+                        {/* <div onClick={() => handleDeleteImage(index)} className='absolute bottom-0 right-0 p-1 bg-red-600 hover:bg-red-600 rounded text-white hidden group-hover:block cursor-pointer'>
+                          <MdDelete />
+                        </div> */}
+                      </div>
+                    )
+                  })
+                }
               </div>
             </div>
 
@@ -88,42 +183,42 @@ const UploadProduct = () => {
             <div>
               <select
                 className='bg-blue-50 border w-full p-2 rounded'
-              // value={selectCategory}
-              // onChange={(e)=>{
-              //   const value = e.target.value 
-              //   const category = allCategory.find(el => el._id === value )
+                value={selectCategory}
+                onChange={(e) => {
+                  const value = e.target.value
+                  const category = allCategory.find(el => el._id === value)
 
-              //   setData((preve)=>{
-              //     return{
-              //       ...preve,
-              //       category : [...preve.category,category],
-              //     }
-              //   })
-              //   setSelectCategory("")
-              // }}
+                  setData((preve) => {
+                    return {
+                      ...preve,
+                      category: [...preve.category, category],
+                    }
+                  })
+                  setSelectCategory("")
+                }}
               >
                 <option value={""}>Select Category</option>
-                {/* {
-                        allCategory.map((c,index)=>{
-                          return(
-                            <option value={c?._id}>{c.name}</option>
-                          )
-                        })
-                      } */}
+                {
+                  allCategory.map((c, index) => {
+                    return (
+                      <option key={index} value={c?._id}>{c.name}</option>
+                    )
+                  })
+                }
               </select>
               <div className='flex flex-wrap gap-3'>
-                {/* {
-                        data.category.map((c,index)=>{
-                          return(
-                            <div key={c._id+index+"productsection"} className='text-sm flex items-center gap-1 bg-blue-50 mt-2'>
-                              <p>{c.name}</p>
-                              <div className='hover:text-red-500 cursor-pointer' onClick={()=>handleRemoveCategory(index)}>
-                                <IoClose size={20}/>
-                              </div>
-                            </div>
-                          )
-                        })
-                      } */}
+                {
+                  data.category.map((c, index) => {
+                    return (
+                      <div key={c._id + index + "productsection"} className='text-sm flex items-center gap-1 bg-blue-50 mt-2'>
+                        <p>{c.name}</p>
+                        <div className='hover:text-red-500 cursor-pointer' onClick={() => handleRemoveCategory(index)}>
+                          <IoClose size={20} />
+                        </div>
+                      </div>
+                    )
+                  })
+                }
               </div>
             </div>
           </div>
@@ -132,42 +227,44 @@ const UploadProduct = () => {
             <div>
               <select
                 className='bg-blue-50 border w-full p-2 rounded'
-              // value={selectSubCategory}
-              // onChange={(e)=>{
-              //   const value = e.target.value 
-              //   const subCategory = allSubCategory.find(el => el._id === value )
+                value={selectSubCategory}
+                onChange={(e) => {
+                  const value = e.target.value
+                  const subCategory = allSubCategory.find(el => el._id === value)
 
-              //   setData((preve)=>{
-              //     return{
-              //       ...preve,
-              //       subCategory : [...preve.subCategory,subCategory]
-              //     }
-              //   })
-              //   setSelectSubCategory("")
-              // }}
+                  setData((preve) => {
+                    return {
+                      ...preve,
+                      subCategory: [...preve.subCategory, subCategory]
+                    }
+                  })
+                  setSelectSubCategory("")
+                }}
               >
                 <option value={""} className='text-neutral-600'>Select Sub Category</option>
-                {/* {
-                        allSubCategory.map((c,index)=>{
-                          return(
-                            <option value={c?._id}>{c.name}</option>
-                          )
-                        })
-                      } */}
+                {
+                  allSubCategory.map((c, index) => {
+                    return (
+                      <option key={index} value={c?._id}>{c.name}</option>
+                    )
+                  })
+                }
               </select>
               <div className='flex flex-wrap gap-3'>
-                {/* {
-                        data.subCategory.map((c,index)=>{
-                          return(
-                            <div key={c._id+index+"productsection"} className='text-sm flex items-center gap-1 bg-blue-50 mt-2'>
-                              <p>{c.name}</p>
-                              <div className='hover:text-red-500 cursor-pointer' onClick={()=>handleRemoveSubCategory(index)}>
-                                <IoClose size={20}/>
-                              </div>
-                            </div>
-                          )
-                        })
-                      } */}
+                {
+                  data.subCategory.map((c, index) => {
+                    return (
+                      <div key={c._id + index + "productsection"} className='text-sm flex items-center gap-1 bg-blue-50 mt-2'>
+                        <p>{c.name}</p>
+                        <div className='hover:text-red-500 cursor-pointer'
+                        // onClick={()=>handleRemoveSubCategory(index)}
+                        >
+                          <IoClose size={20} />
+                        </div>
+                      </div>
+                    )
+                  })
+                }
               </div>
             </div>
           </div>
@@ -179,7 +276,7 @@ const UploadProduct = () => {
               type='text'
               placeholder='Enter product unit'
               name='unit'
-              // value={data.unit}
+              value={data.unit}
               // onChange={handleChange}
               required
               className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
@@ -193,7 +290,7 @@ const UploadProduct = () => {
               type='number'
               placeholder='Enter product stock'
               name='stock'
-              // value={data.stock}
+              value={data.stock}
               // onChange={handleChange}
               required
               className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
@@ -207,7 +304,7 @@ const UploadProduct = () => {
               type='number'
               placeholder='Enter product price'
               name='price'
-              // value={data.price}
+              value={data.price}
               // onChange={handleChange}
               required
               className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
@@ -221,7 +318,7 @@ const UploadProduct = () => {
               type='number'
               placeholder='Enter product discount'
               name='discount'
-              // value={data.discount}
+              value={data.discount}
               // onChange={handleChange}
               required
               className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
